31814e7dcb4e3241dd613ce369593be12405f326
AlberGonAcosta <ninotyriel@gmail.com>
2015-01-18 18:52:19 +0000

    Implementación de los TableModel

--- RecetApp/src/dad/recetapp/ui/CategoriasTableModel.java
+++ RecetApp/src/dad/recetapp/ui/CategoriasTableModel.java
@@ -1,5 +1,52 @@
 package dad.recetapp.ui;
 
-public class CategoriasTableModel {
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.swing.table.AbstractTableModel;
+
+import dad.recetapp.services.items.CategoriaItem;
+
+@SuppressWarnings("serial")
+public class CategoriasTableModel extends AbstractTableModel{
+
+	private List<CategoriaItem> categorias = new ArrayList<CategoriaItem>();
+	private static final String [] COLUMN_NAMES = {"Descripción"};
+	private static final Class<?> [] COLUMN_CLASSES = {String.class};
+	
+	public void setIngredientes(List<CategoriaItem> categorias){
+		this.categorias = categorias;
+		fireTableDataChanged();
+	}
+	
+	public Class<?> getColumnClass(int columnIndex) {
+		return COLUMN_CLASSES[columnIndex];
+	}
+
+	public int getColumnCount() {
+		return COLUMN_NAMES.length;
+	}
+
+	public int getRowCount() {
+		return categorias.size();
+	}
+
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		Object value = null;
+		CategoriaItem categoria = categorias.get(rowIndex);
+		switch(columnIndex){
+		case 0: value = categoria.getDescripcion(); break;
+		}
+		return value;
+	}
+	
+	@Override
+	public String getColumnName(int columnIndex) {
+		return COLUMN_NAMES[columnIndex];
+	}
+
+	@Override
+	public boolean isCellEditable(int rowIndex, int columnIndex) {
+		return false;
+	}
 }
--- RecetApp/src/dad/recetapp/ui/IngredientesTableModel.java
+++ RecetApp/src/dad/recetapp/ui/IngredientesTableModel.java
@@ -1,5 +1,52 @@
 package dad.recetapp.ui;
 
-public class IngredientesTableModel {
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.swing.table.AbstractTableModel;
+
+import dad.recetapp.services.items.IngredienteItem;
+
+@SuppressWarnings("serial")
+public class IngredientesTableModel extends AbstractTableModel {
+
+	private List<IngredienteItem> ingredientes = new ArrayList<IngredienteItem>();
+	private static final String [] COLUMN_NAMES = {"Nombre"};
+	private static final Class<?> [] COLUMN_CLASSES = {String.class};
+	
+	public void setIngredientes(List<IngredienteItem> ingrediente){
+		this.ingredientes = ingrediente;
+		fireTableDataChanged();
+	}
+	
+	public Class<?> getColumnClass(int columnIndex) {
+		return COLUMN_CLASSES[columnIndex];
+	}
+
+	public int getColumnCount() {
+		return COLUMN_NAMES.length;
+	}
+
+	public int getRowCount() {
+		return ingredientes.size();
+	}
+
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		Object value = null;
+		IngredienteItem ingrediente = ingredientes.get(rowIndex);
+		switch(columnIndex){
+		case 0: value = ingrediente.getTipo().getNombre(); break;
+		}
+		return value;
+	}
+	
+	@Override
+	public String getColumnName(int columnIndex) {
+		return COLUMN_NAMES[columnIndex];
+	}
+
+	@Override
+	public boolean isCellEditable(int rowIndex, int columnIndex) {
+		return false;
+	}
 }
